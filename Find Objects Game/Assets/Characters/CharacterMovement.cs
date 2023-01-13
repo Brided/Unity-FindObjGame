6caa38c06ba7939c324212bcbe5bdddce0da5bb5
Steven <ungsteven6@gmail.com>
2023-01-13 12:52:16 +0100

    Updated with a tree

--- Find Objects Game/Assets/Characters/CharacterMovement.cs
+++ Find Objects Game/Assets/Characters/CharacterMovement.cs
@@ -4,37 +4,31 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float speed = 6.0f; // The character's movement speed
-    private Vector3 direction;
-    private Vector3 moveDirection; // The direction the character will move in
-    public float rotationSpeed = 90f; // A variable to control the rotation speed
+    public float speed = 6.0f;
+    public float rotationSpeed = 90f;
 
-    private CharacterController characterController; // A reference to the CharacterController component
-    private Camera camera;
+    private CharacterController characterController;
+    private Transform camera;
 
-    // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        camera = Camera.main;
+        camera = Camera.main.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        moveDirection = new Vector3(horizontal, 0, vertical);
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        // Normalize the moveDirection vector
-        moveDirection = moveDirection.normalized;
-        // Move the character in the moveDirection vector
-        characterController.Move(moveDirection * speed * Time.deltaTime);
+        float targAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
 
-        if (moveDirection != Vector3.zero) {
-            // Rotate the character towards the moveDirection
-            Quaternion rotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        if (direction.magnitude >= 0.1f) {
+            Vector3 moveDirection = Quaternion.Euler(0f, targAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection * speed * Time.deltaTime);
+
+            transform.rotation = Quaternion.Euler(0f, targAngle, 0f);
         }
     }
 }
